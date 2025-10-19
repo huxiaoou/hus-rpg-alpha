58e264d86fb1192222baa0631551d906321dba34
huxiaoou <516984451@qq.com>
2025-10-19 18:31:57 +0800

    Add camera control

---
+++ scenes/camera_control.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+class_name CameraControl
+
+@export var view_lim_left_top: Vector2i = Vector2i(0, 0)
+@export var view_lim_right_down: Vector2i = Vector2i(1920 * 2, 1080)
+
+@export var camera_move_speed: float = 500
+@onready var camera_2d: Camera2D = $Camera2D
+
+var lim_left_top: Vector2i
+var lim_right_down: Vector2i
+
+
+func _ready() -> void:
+	lim_left_top = view_lim_left_top + get_viewport().size / 2
+	lim_right_down = view_lim_right_down - get_viewport().size / 2
+
+
+func _process(delta: float) -> void:
+	var camera_mov_direction: Vector2 = Input.get_vector("camera_mov_left", "camera_mov_right", "camera_mov_up", "camera_mov_down").normalized()
+	var target_position: Vector2 = global_position + camera_mov_direction * camera_move_speed * delta
+	target_position.x = clamp(target_position.x, lim_left_top.x, lim_right_down.x)
+	target_position.y = clamp(target_position.y, lim_left_top.y, lim_right_down.y)
+	global_position = target_position
+	return
--- scenes/unit_test.gd
+++ scenes/unit_test.gd
@@ -6,7 +6,13 @@ var speed: float = 100
 var direction_move: Vector2 = Vector2.ZERO
 
 
-func _process(delta: float) -> void:
-	direction_move = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction_move * speed * delta
+func _process(_delta: float) -> void:
+	direction_move = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+	if direction_move.x > 0:
+		direction_move = Vector2.RIGHT
+	elif direction_move.x < 0:
+		direction_move = Vector2.LEFT
+	else:
+		direction_move = Vector2.ZERO
+	velocity = direction_move * speed
 	move_and_slide()
