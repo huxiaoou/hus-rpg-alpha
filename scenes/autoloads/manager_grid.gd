5782af245121f25da4d36f3a49fdfe34ca478fd1
huxiaoou <516984451@qq.com>
2025-10-19 23:25:12 +0800

    Add path finding system

--- scenes/autoloads/manager_grid.gd
+++ scenes/autoloads/manager_grid.gd
@@ -13,3 +13,17 @@ func get_world_pos(grid_pos: Vector2i) -> Vector2:
 
 func get_mouse_grid_pos() -> Vector2i:
 	return get_grid_pos(layer_nav.get_global_mouse_position())
+
+# --- path finding
+
+
+func get_nav_grid_path(start_grid_pos: Vector2i, end_grid_pos: Vector2i) -> Array[Vector2i]:
+	return layer_nav.a_star.get_id_path(start_grid_pos, end_grid_pos)
+
+
+func get_nav_world_path(start_grid_pos: Vector2i, end_grid_pos: Vector2i) -> Array[Vector2]:
+	var grid_path: Array[Vector2i] = get_nav_grid_path(start_grid_pos, end_grid_pos)
+	var world_path: Array[Vector2]
+	for grid_pos in grid_path:
+		world_path.append(ManagerGrid.get_world_pos(grid_pos))
+	return world_path
--- scenes/units/unit_test.gd
+++ scenes/units/unit_test.gd
@@ -5,20 +5,31 @@ class_name UnitTest
 @export var start_grid: Vector2i
 
 var speed: float = 500
-var direction_move: Vector2 = Vector2.ZERO
+var target_world_pos: Vector2
+var grid_pos: Vector2i:
+	get:
+		return ManagerGrid.get_grid_pos(global_position)
+var path: Array[Vector2]
 
 
 func _ready() -> void:
+	target_world_pos = ManagerGrid.get_world_pos(start_grid)
 	global_position = ManagerGrid.get_world_pos(start_grid)
 
 
-func _process(_delta: float) -> void:
-	direction_move = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
-	if direction_move.x > 0:
-		direction_move = Vector2.RIGHT
-	elif direction_move.x < 0:
-		direction_move = Vector2.LEFT
-	else:
-		direction_move = Vector2.ZERO
-	velocity = direction_move * speed
-	move_and_slide()
+func _process(delta: float) -> void:
+	if path and not path.is_empty():
+		move(path[0], delta)
+		if global_position == path[0]:
+			path.remove_at(0)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_mouse_click"):
+		var mouse_grid_pos: Vector2i = ManagerGrid.get_mouse_grid_pos()
+		target_world_pos = ManagerGrid.get_world_pos(mouse_grid_pos)
+		path = ManagerGrid.get_nav_world_path(self.grid_pos, mouse_grid_pos)
+
+
+func move(target_world_pos: Vector2, delta: float) -> void:
+	global_position = global_position.move_toward(target_world_pos, speed * delta)
