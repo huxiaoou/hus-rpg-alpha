bf53c2e9f7b7a0162310080fb5dc1f1e304cdcfd
huxiaoou <516984451@qq.com>
2025-10-19 15:48:09 +0800

    Add plugin formatter

--- scenes/unit_test.gd
+++ scenes/unit_test.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
+
 class_name UnitTest
 
 var speed: float = 100
 var direction_move: Vector2 = Vector2.ZERO
 
+
 func _process(delta: float) -> void:
 	direction_move = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = direction_move * speed
+	velocity = direction_move * speed * delta
 	move_and_slide()
-	
