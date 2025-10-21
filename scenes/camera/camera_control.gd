44dedb5cee76a156a3e565a64ddff6de18c12386
huxiaoou <516984451@qq.com>
2025-10-21 23:52:00 +0800

    Smooth camera movements

--- scenes/camera/camera_control.gd
+++ scenes/camera/camera_control.gd
@@ -4,13 +4,14 @@ class_name CameraControl
 
 @export var view_lim_left_top: Vector2i = Vector2i(0, 0)
 @export var view_lim_right_down: Vector2i = Vector2i(1920 * 2, 1080)
-@export var camera_move_speed: float = 500
+@export var camera_move_speed: float = 1500
 
 @onready var camera_2d: Camera2D = $Camera2D
 
 var lim_left_top: Vector2i
 var lim_right_down: Vector2i
 var tracking_unit: UnitTest = null
+var target_position: Vector2
 
 
 func _ready() -> void:
@@ -22,12 +23,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var camera_mov_direction: Vector2 = Input.get_vector("camera_mov_left", "camera_mov_right", "camera_mov_up", "camera_mov_down").normalized()
 	if camera_mov_direction != Vector2.ZERO:
-		var target_position: Vector2 = global_position + camera_mov_direction * camera_move_speed * delta
-		target_position.x = clamp(target_position.x, lim_left_top.x, lim_right_down.x)
-		target_position.y = clamp(target_position.y, lim_left_top.y, lim_right_down.y)
-		global_position = target_position
 		tracking_unit = null
-	track_unit()
+		target_position = global_position + camera_mov_direction * camera_move_speed * delta
+		clamp_target_pos()
+		track_target()
+	elif tracking_unit != null:
+		target_position = global_position.move_toward(tracking_unit.global_position, camera_move_speed * delta)
+		clamp_target_pos()
+		track_target()
 	return
 
 
@@ -36,7 +39,12 @@ func on_unit_double_clicked(unit: UnitTest) -> void:
 	return
 
 
-func track_unit() -> void:
-	if tracking_unit != null:
-		global_position.x = clamp(tracking_unit.global_position.x, lim_left_top.x, lim_right_down.x)
-		global_position.y = clamp(tracking_unit.global_position.y, lim_left_top.y, lim_right_down.y)
+func clamp_target_pos() -> void:
+	target_position.x = clamp(target_position.x, lim_left_top.x, lim_right_down.x)
+	target_position.y = clamp(target_position.y, lim_left_top.y, lim_right_down.y)
+	return
+
+
+func track_target() -> void:
+	global_position = target_position
+	return
