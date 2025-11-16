a8d6283cd357d88c0914f6db802b46cbd6fce0d4
huxiaoou <516984451@qq.com>
2025-11-16 20:47:52 +0800

    Refactor Scene EffectImpact

--- scenes/effects/effect_impact.gd
+++ scenes/effects/effect_impact.gd
@@ -1,6 +1,24 @@
-extends AnimatedSprite2D
+extends Node2D
 
 class_name EffectImpact
 
+@export var effect_name: String
+@export var effect_audio: AudioStream
+@export var duration_milliseconds: int = 1000
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
+var bgn_time: int = 0
+
+
 func _ready() -> void:
-	animation_finished.connect(queue_free)
+	animated_sprite_2d.animation = effect_name
+	audio_stream_player_2d.stream = effect_audio
+	bgn_time = Time.get_ticks_msec()
+
+
+func _process(delta: float) -> void:
+	var cur_time: int = Time.get_ticks_msec()
+	if cur_time >= bgn_time + duration_milliseconds:
+		queue_free()
