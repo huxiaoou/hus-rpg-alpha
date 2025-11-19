a561b11a51b55fff5a12ade717f5415bfc0a3078
huxiaoou <516984451@qq.com>
2025-11-19 23:06:00 +0800

    Build UI

---
+++ scenes/ui/bar_status.gd
@@ -0,0 +1,16 @@
+extends ProgressBar
+
+class_name BarStatus
+
+@export var color: Color
+@onready var label: Label = $Label
+
+
+func _ready() -> void:
+	var sb: StyleBoxFlat = StyleBoxFlat.new()
+	add_theme_stylebox_override("fill", sb)
+	sb.bg_color = color
+	sb.border_width_top = 2
+	sb.border_width_bottom = 2
+	sb.border_width_left = 2
+	sb.border_color = Color("cccccc")
