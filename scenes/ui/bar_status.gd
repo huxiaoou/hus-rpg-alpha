b5e13e4bbe13142a85dfcdb7cb03a1d7af2a58da
huxiaoou <516984451@qq.com>
2025-11-21 23:16:45 +0800

    Further developed UI

--- scenes/ui/bar_status.gd
+++ scenes/ui/bar_status.gd
@@ -4,18 +4,38 @@ class_name BarStatus
 
 @export var color: Color
 @onready var label: Label = $Label
+@onready var progress_bar_front: ProgressBar = $ProgressBarFront
+
+var styleboxflat_back: StyleBoxFlat = null
+var styleboxflat_front: StyleBoxFlat = null
 
 
 func _ready() -> void:
-	var sb: StyleBoxFlat = StyleBoxFlat.new()
-	add_theme_stylebox_override("fill", sb)
-	sb.bg_color = color
-	sb.border_width_top = 2
-	sb.border_width_bottom = 2
-	sb.border_width_left = 2
-	sb.border_color = Color("cccccc")
+	styleboxflat_back = get_theme_stylebox("fill")
+	styleboxflat_front = StyleBoxFlat.new()
+	progress_bar_front.add_theme_stylebox_override("fill", styleboxflat_front)
+	styleboxflat_front.bg_color = color
+	styleboxflat_front.border_width_left = 2
+	styleboxflat_front.border_width_top = 2
+	styleboxflat_front.border_width_bottom = 2
 
 
-func update(new_value: float) -> void:
-	value = new_value
+func update_max_val(new_max_value: float) -> void:
+	progress_bar_front.max_value = new_max_value
+	max_value = new_max_value
+	update_label()
+
+
+func update_val(new_value: float) -> void:
+	progress_bar_front.value = new_value
+	await get_tree().create_timer(0.5).timeout
+	var tween: Tween = get_tree().create_tween()
+	for __ in range(3):
+		tween.tween_property(styleboxflat_back, "bg_color", Color(1, 1, 1, 0), 0.1)
+		tween.tween_property(styleboxflat_back, "bg_color", Color(1, 1, 1, 1), 0.1)
+	tween.tween_property(self, "value", new_value, 0.5)
+	tween.tween_callback(update_label)
+
+
+func update_label() -> void:
 	label.text = "%d/%d" % [value, max_value]
--- scenes/ui/ui_portrait.gd
+++ scenes/ui/ui_portrait.gd
@@ -16,29 +16,29 @@ var player: UnitTest = null:
 		player.stamina_changed.connect(on_staminc_changed)
 		player.resolve_changed.connect(on_resolve_changed)
 
-		bar_health.max_value = player.max_health
-		bar_magicka.max_value = player.max_magicka
-		bar_stamina.max_value = player.max_stamina
-		bar_resolve.max_value = player.max_resolve
+		bar_health.update_max_val(player.max_health)
+		bar_magicka.update_max_val(player.max_magicka)
+		bar_stamina.update_max_val(player.max_stamina)
+		bar_resolve.update_max_val(player.max_resolve)
 
-		bar_health.update(player.cur_health)
-		bar_magicka.update(player.cur_magicka)
-		bar_stamina.update(player.cur_stamina)
-		bar_resolve.update(player.cur_resolve)
+		bar_health.update_val(player.cur_health)
+		bar_magicka.update_val(player.cur_magicka)
+		bar_stamina.update_val(player.cur_stamina)
+		bar_resolve.update_val(player.cur_resolve)
 		texture_rect.texture = player.potrait
 
 
 func on_health_changed(new_health: float) -> void:
-	bar_health.update(new_health)
+	bar_health.update_val(new_health)
 
 
 func on_magicka_changed(new_magicka: float) -> void:
-	bar_magicka.update(new_magicka)
+	bar_magicka.update_val(new_magicka)
 
 
 func on_staminc_changed(new_stamina: float) -> void:
-	bar_stamina.update(new_stamina)
+	bar_stamina.update_val(new_stamina)
 
 
 func on_resolve_changed(new_resolve: float) -> void:
-	bar_resolve.update(new_resolve)
+	bar_resolve.update_val(new_resolve)
